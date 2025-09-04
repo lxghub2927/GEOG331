bc287259c5200be88a34ac98fc93655bde9f7264
lxghub2927 <zwang2@colgate.edu>
2025-09-04 11:31:11 -0400

    ativity2

--- Activity1.R
+++ Activity1.R
@@ -1,1 +1,14 @@
 print ("Hello World")
+#test operations according to activity 1
+6^6
+234+8
+15-23
+a <- 6234
+a*5
+a/3
+b <- c(2395,82,2947)
+b/2
+q2vector <- c(114,514,1919,810,2025,150,1989)
+q2vector/2
+q2vector*6.4
+q2vector+999
