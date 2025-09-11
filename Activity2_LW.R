ca4d19748094f268fef8f1016d67974a80f5e72d
lxghub2927 <zwang2@colgate.edu>
2025-09-11 11:14:01 -0400

    meiow -9/11/25 1133

---
+++ Activity2_LW.R
@@ -0,0 +1,18 @@
+#New filed for activity/hw2
+#make a vector of tree heights in meters
+heights <- c(30,41,20,22)
+#c is a concotanate
+#covert heights to cm
+h_cm <- heights*100
+h_cm
+
+#matric\es
+help (matrix)
+
+#function inside function -> matrices
+#by row
+mat <- matrix (c(1,2,3,4,5,6), ncol=2, byrow = TRUE)
+mat
+#by colum
+mat_col <- matrix (c(1,2,3,4,5,6), ncol=2, byrow = FALSE)
+mat_col
