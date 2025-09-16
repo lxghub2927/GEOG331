1f2ddc3669c86fc01993c71405fe06d7e9cbf52d
lxghub2927 <zwang2@colgate.edu>
2025-09-16 11:13:02 -0400

    9/16/25 update

--- Activity2_LW.R
+++ Activity2_LW.R
@@ -25,3 +25,29 @@ datW <- read.csv("Z:\\zwang2\\github_lw\\GEOG331\\data\\NOAA\\noaa_weather\\2011
 str(datW)
 #9 variables are seen suggesting there are 9 columns
 #according to data, there are 157849 objcts. This suggests there are 157849 rows.
+
+#specify a column with a proper date format <- copied from instructions
+#note the format here dataframe$column
+datW$dateF <- as.Date(datW$DATE, "%Y-%m-%d")
+#google date formatting in r to find more options and learn more
+
+#find out all unique site names
+unique(datW$NAME)
+#To find mean maximum temp for Aberdeen, WA
+mean(datW$TMAX[datW$NAME == "ABERDEEN, WA US"])
+#NA suggests data is missing, we have to ignore the NA data here
+#Use argument to ignore -> Look at the mean maximum temperature for Aberdeen
+#with na.rm argument set to true to ignore NA
+mean(datW$TMAX[datW$NAME == "ABERDEEN, WA US"], na.rm=TRUE)
+
+#calculate the average daily temperature <- "Since this is the average across many decades of observations, this value can help tell us about the climate of the site. Before we move on, average daily temperature is often more helpful to evaulate temperature. The average is always halfway between the minimum and maximum.
+#This temperature will be halfway between the minimum and maximum temperature" (Prof Loranty, activity 2)
+datW$TAVE <- datW$TMIN + ((datW$TMAX-datW$TMIN)/2)
+
+#get the mean across all sites
+#the by function is a list of one or more variables to index over.
+#FUN indicates the function we want to use
+#if you want to specify any function specific arguments use a comma and add them after the function
+#here we want to use the na.rm arguments specific to 
+averageTemp <- aggregate(datW$TAVE, by=list(datW$NAME), FUN="mean",na.rm=TRUE)
+averageTemp
