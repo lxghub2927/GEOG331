51026955213e984d12ef2610bb4b534fdf43a29b
lxghub2927 <zwang2@colgate.edu>
2025-09-27 18:03:07 -0400

    activity 3 9/27/25 redon

--- Activity3_LW.R
+++ Activity3_LW.R
@@ -1,4 +1,6 @@
 #Activity 3: GEOG 331: Luke Wang
+#Testing your codes
+
 #create a function. The names of the arguments for your function will be in parentheses. Everything in curly brackets will be run each time the function is run.
 assert <- function(statement,err.message){
   #if evaluates if a statement is true or false for a single item
@@ -53,8 +55,9 @@ assert <- function(statement,err.message){
     print(err.message)
   }
 }
+#the following segment incorporates converting the times of the data
 
-#convert to standardized format
+#convert to standardized format <- standard: new york
 #date format is m/d/y
 dates <- mdy_hm(datW$timestamp, tz= "America/New_York")
 #calculate day of year
@@ -89,12 +92,42 @@ plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
 plot(datW$DD, datW$air.temperature, pch=19, type="b", xlab = "Day of Year",
      ylab="Air temperature (degrees C)")
 
-#I'm going to make a new column to work with that indicates that I am conducting QAQC
-#because overwriting values should be done cautiously and can lead to confusing issues.
-#It can be particularly confusing when you are just learning R.
-#Here I'm using the ifelse function
-#the first argument is a logical statement to be evaluated as true or false on a vector
-#the second argument is the value that my air.tempQ1 column will be given if the statement
-#is true. The last value is the value that will be given to air.tempQ1 if the statement is false.
-#In this case it is just given the air temperature value
+#the following segment incorporates how to remove false anomalous datas.
 datW$air.tempQ1 <- ifelse(datW$air.temperature < 0, NA, datW$air.temperature)
+
+#checking for realistic values:
+#check the values at the extreme range of the data
+#and throughout the percentiles
+quantile(datW$air.tempQ1)
+
+#look at days with really low air temperature
+datW[datW$air.tempQ1 < 8,]  
+#look at days with really high air temperature
+datW[datW$air.tempQ1 > 33,]
+
+#chapter: measurements outside of sensor capabilities
+
+'''
+The following segment describes using lightning sensors and precipitation sensors to detect thunderstorms. In turn, this points out data collected during thunderstorms
+that might be unreliable due to inaccuracy from heavy rain and strong wind
+'''
+#plot precipitation and lightning strikes on the same plot
+#normalize lighting strikes to match precipitation
+lightscale <- (max(datW$precipitation)/max(datW$lightning.acvitivy)) * datW$lightning.acvitivy
+#make the plot with precipitation and lightning activity marked
+#make it empty to start and add in features
+plot(datW$DD , datW$precipitation, xlab = "Day of Year", ylab = "Precipitation & lightning",
+     type="n")
+#plot precipitation points only when there is precipitation 
+#make the points semi-transparent
+points(datW$DD[datW$precipitation > 0], datW$precipitation[datW$precipitation > 0],
+       col= rgb(95/255,158/255,160/255,.5), pch=15)        
+
+#plot lightning points only when there is lightning     
+points(datW$DD[lightscale > 0], lightscale[lightscale > 0],
+       col= "tomato3", pch=19)
+
+
+#For Question 5
+#Using assert to see if lightscale is in fact incorporating the lightning.activity values. Assert functions checks this by testing to see if data length is same for both group of values.
+assert(length(lightscale) == length(datW$lightning.acvitivy))
