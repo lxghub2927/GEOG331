4e876ed3af92495cf3eb31f4924516788855cae8
Zixia (Luke) Wang '27 <zwang2@colgate.edu>
2025-11-03 00:32:59 -0500

    "stop children what's that sound" 11/3 12:32AM

--- Activity5_LW.R
+++ Activity5_LW.R
@@ -190,7 +190,7 @@ datD_24days
 datD_24days$dt
 names(datD_24days)
 grep("^dt", names(datD_24days), value = TRUE)  # look for dt, dt.x, dt.y, etc.
-
+datD_24days$dt <- as_datetime(as.Date(datD_24days$date, format="%m/%d/%Y"), tz=TZ) + hm(datD_24days$time)
 
 plot(datD_24days$dt, datD_24days$discharge, type = "l",
      xlab = "Date",
@@ -202,5 +202,68 @@ plot(datD_24days$dt, datD_24days$discharge, type = "l",
 points(datD_24days$dt[datD_24days$full_day],
        datD_24days$discharge[datD_24days$full_day],
        col = "orange", pch = 16)
-################
+
+###################
+
+#subsest discharge and precipitation within range of interest
+hydroD <- datD[datD$doy >= 248 & datD$doy < 250 & datD$year == 2011,]
+hydroP <- datP[datP$doy >= 248 & datP$doy < 250 & datP$year == 2011,]
+min(hydroD$discharge)
+
+
+#self-debug attempt-> The codes provided here does not work for me!
+hydroP$HPCP <- suppressWarnings(as.numeric(as.character(hydroP$HPCP)))
+hydroP$HPCP[hydroP$HPCP %in% c(-9999, -999, -99, 9999, 999)] <- NA  # adjust as needed
+
+#get minimum and maximum range of discharge to plot
+#go outside of the range so that it's easy to see high/low values
+#floor rounds down the integer
+yl <- floor(min(hydroD$discharge))-1
+yl
+#ceiling rounds up to the integer
+yh <- ceiling(max(hydroD$discharge))+1
+yh
+#minimum and maximum range of precipitation to plot
+pl <- 0
+pl
+pm <-  ceiling(max(hydroP$HPCP))+.5
+pm
+nrow(hydroP)
+#scale precipitation to fit on the 
+hydroP$pscale <- (((yh-yl)/(pm-pl)) * hydroP$HPCP) + yl
+
+
+
+par(mai=c(1,1,1,1))
+#make plot of discharge
+plot(hydroD$decDay,
+     hydroD$discharge, 
+     type="l", 
+     ylim=c(yl,yh), 
+     lwd=2,
+     xlab="Day of year", 
+     ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")))
+#add bars to indicate precipitation 
+for(i in 1:nrow(hydroP)){
+  polygon(c(hydroP$decDay[i]-0.017,hydroP$decDay[i]-0.017,
+            hydroP$decDay[i]+0.017,hydroP$decDay[i]+0.017),
+          c(yl,hydroP$pscale[i],hydroP$pscale[i],yl),
+          col=rgb(0.392, 0.584, 0.929,.2), border=NA)
+}
+
+###################
+###Test Q9
+library(ggplot2)
+#specify year as a factor
+datD$yearPlot <- as.factor(datD$year)
+#make a boxplot
+ggplot(data= datD, aes(yearPlot,discharge)) + 
+  geom_boxplot()
+
+#make a violin plot
+ggplot(data= datD, aes(yearPlot,discharge)) + 
+  geom_violin()
+
+#####Q9#####
+
 
